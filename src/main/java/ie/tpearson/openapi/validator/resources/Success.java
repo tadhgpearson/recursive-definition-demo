d8412d9ce9c797e0adc89e1909e575a180d9dd2a
Tadhg Pearson <tpearson@akamai.com>
2020-06-17 09:32:09 -0400

    Demonstrate UUID format issue

---
+++ src/main/java/ie/tpearson/openapi/validator/resources/Success.java
@@ -0,0 +1,13 @@
+package ie.tpearson.openapi.validator.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Success {
+
+  private String id;
+}
--- src/test/java/ie/tpearson/openapi/validator/resources/OpenAPIValidatorTest.java
+++ src/test/java/ie/tpearson/openapi/validator/resources/OpenAPIValidatorTest.java
@@ -8,11 +8,17 @@ public class OpenAPIValidatorTest {
 
   private final OpenAPIValidator underTest = OpenAPIValidator.from("recursive-definition.yaml");
 
-  @Test public void testValidResponse() {
+  @Test public void testValidErrorResponse() {
     ProblemDetails response =
       ProblemDetails.builder().status(400).type("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400").title("Sample error").build();
     underTest.validateResponse("/", "retrieveApplicationCollection", 400, response);
     assertTrue(true);
+  }
+
+  @Test public void testValidSuccessResponse(){
+    Success response = new Success("fa3a04be-1a06-449b-b209-d920eb6c6e5b");
+    underTest.validateResponse("/", "retrieveApplicationCollection", 200, response);
+
   }
 
 }
